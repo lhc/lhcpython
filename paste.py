57b1465e88755e3b1078cd071b8d48b50f6d2d54
Joao S. O. Bueno <gwidion@gmail.com>
2016-01-26 21:59:35 -0200

    refatorado para usar funções

--- paste.py
+++ paste.py
@@ -18,12 +18,22 @@ def checa_delimitador():
     del sys.argv[i]
     del sys.argv[i - 1]
 
-checa_delimitador()
 
-if len(sys.argv) < 3:
-    print("Passe pelo menos dois nomes de arquivo", file=sys.stderr)
-    sys.exit(1)
+def checa_parametros():
+    if len(sys.argv) < 3:
+        print("Passe pelo menos dois nomes de arquivo", file=sys.stderr)
+        sys.exit(1)
+    return sys.argv[1:3]
 
-with open(sys.argv[1]) as arq1,  open(sys.argv[2]) as arq2:
-    for linha1, linha2 in zip(arq1, arq2):
-        print (linha1.strip("\n"), linha2.strip("\n"), sep=DELIMITADOR)
+def principal(arq1_nome, arq2_nome):
+    with open(arq1_nome) as arq1,  open(arq2_nome) as arq2:
+        for linha1, linha2 in zip(arq1, arq2):
+            print (linha1.strip("\n"), linha2.strip("\n"), sep=DELIMITADOR)
+
+def principal_mesmo():
+    checa_delimitador()
+    arq1, arq2 = checa_parametros()
+    principal(arq1, arq2)
+
+if __name__ == "__main__":
+    principal_mesmo()
