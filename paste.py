6756c31422ff139e89cf53399d3ab70971b07558
Joao S. O. Bueno <gwidion@gmail.com>
2016-01-26 21:45:41 -0200

    Usando zip, e mudando o delimitador

--- paste.py
+++ paste.py
@@ -5,23 +5,25 @@ import sys
 
 DELIMITADOR = "\t"
 
+def checa_delimitador():
+    global DELIMITADOR
+    previous = ""
+    for i, parametro in enumerate(sys.argv):
+        if previous == "-d":
+            DELIMITADOR = parametro
+            break
+        previous = parametro
+    else:
+        return
+    del sys.argv[i]
+    del sys.argv[i - 1]
+
+checa_delimitador()
+
 if len(sys.argv) < 3:
     print("Passe pelo menos dois nomes de arquivo", file=sys.stderr)
     sys.exit(1)
 
-def le_linha(arq):
-    arq_acabou = False
-    try:
-        linha = next(arq)
-    except StopIteration:
-        arq_acabou = True
-        linha = ""
-    return linha, arq_acabou
-
-
 with open(sys.argv[1]) as arq1,  open(sys.argv[2]) as arq2:
-    arq1_acabou  = arq2_acabou = False
-    while not (arq1_acabou and arq2_acabou):
-        linha1, arq1_acabou = le_linha(arq1)
-        linha2, arq2_acabou = le_linha(arq2)
+    for linha1, linha2 in zip(arq1, arq2):
         print (linha1.strip("\n"), linha2.strip("\n"), sep=DELIMITADOR)
